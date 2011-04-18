0b669761333d245a842b9d8e28b1722a3a18fe4f
wills <wills@ubuntu.(none)>
2011-04-18 16:54:54 +0800

    add the dnotify to client.py

--- client.py
+++ client.py
@@ -1,27 +1,19 @@
 #-*- coding:utf8 -*-
 #!/usr/bin/env python
-
-
 ########################################################################
 #author:liutaihua
 #email: defage@gmail.com
 #
 #########################################################################
-"""
-Usage:
-    [-h|--help] [-H|--host] [-p|--port] [-f|--file]
 
--H|--host    the scribe host
--p|--port    the scribe port
--f|--file    the log file
 
-example:
-    python client -H 127.0.0.1 -p 1463 -f /tmp/test.log
-"""
-
-import sys
+import os, sys
 import time
-import getopt
+import syslog
+import threading
+from defage.daemon import Daemon
+
+from  defage.pyinotify import  WatchManager, Notifier, ProcessEvent, IN_DELETE, IN_CREATE,IN_MODIFY
 
 from scribe import scribe
 from thrift.transport import TTransport, TSocket
@@ -29,12 +21,8 @@ from thrift.protocol import TBinaryProtocol
 
 
 
-
-#host = '127.0.0.1'
-#port = 1463
-
-
-
+host = '10.241.12.74'
+port = 1463
 
 def getLocalIp():
     from socket import socket, SOCK_DGRAM, AF_INET
@@ -45,52 +33,84 @@ def getLocalIp():
     return LocalIp
 
 
-def main(argv):
-    if not argv:
-        usage()
-        sys.exit()
-    try:
-        opts, args = getopt.getopt(argv, "hH:p:f:", ["help", "host=","port=","logfile="])
-    except getopt.GetoptError,err:
-        print err
-        usage()
-        sys.exit()
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif opt in ("-H", "--host"):
-            host = arg
-        elif opt in ("-p", "--port"):
-            port = arg
-        elif opt in ("-f", "--file"):
-            logfile = arg
-        else:
-            pass
-            usage()
-            sys.exit()
-    category_name = getLocalIp() + "_" + logfile.split('/')[-2] + "_" + logfile.split('/')[-1]
+class TailThread(threading.Thread):
+    def __init__(self, host, port, handleFile):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.handleFile= handleFile
 
-    socket = TSocket.TSocket(host=host, port=port)
-    transport = TTransport.TFramedTransport(socket)
-    protocol = TBinaryProtocol.TBinaryProtocol(trans=transport, strictRead=False, strictWrite=False)
-    client = scribe.Client(iprot=protocol, oprot=protocol)
-    transport.open()
-    file = open(logfile,'r')
-    file.seek(0,2)
-    while True:
-        where = file.tell()
-        line = file.readline()
-        if not line:
-    	    time.sleep(1)
-	    file.seek(where)
-        else:
-	    print line, # already has newline
-	    log_entry = scribe.LogEntry(category=category_name, message=line)
-	    result = client.Log(messages=[log_entry])
-	    #transport.close()
-def usage():
-    print __doc__
+    def run(self):
+        category_name = "[" + getLocalIp() +  "]" + "[" + self.handleFile.split('/')[-2] + "]" + "[" + self.handleFile.split('/')[-1] + "]"
+        socket = TSocket.TSocket(host=self.host, port=self.port)
+        transport = TTransport.TFramedTransport(socket)
+        protocol = TBinaryProtocol.TBinaryProtocol(trans=transport, strictRead=False, strictWrite=False)
+        client = scribe.Client(iprot=protocol, oprot=protocol)
+        file = open(self.handleFile,'r')
+        file.seek(0,2)
+
+        transport.open()
+        while True:
+            where = file.tell()
+            line = file.readline()
+            if not line:
+                time.sleep(1)
+                file.seek(where)
+            else:
+                print line, # already has newline
+                syslog.syslog(line)
+                log_entry = scribe.LogEntry(category=category_name, message=line)
+                result = client.Log(messages=[log_entry])
+                #transport.close()
+
+class EventHandler(ProcessEvent):
+    def process_IN_CREATE(self, event):
+        print   "Create file: %s "  %   os.path.join(event.path, event.name)
+        thread = TailThread(host, port, handleFile=os.path.join(event.path, event.name))
+        thread.start()
+
+    def process_IN_DELETE(self, event):
+        print   "Delete file: %s "  %   os.path.join(event.path, event.name)
+    
+    def process_IN_MODIFY(self, event):
+        print   "Modify file: %s "  %   os.path.join(event.path, event.name)
+        thread = TailThread(host, port, handleFile=os.path.join(event.path, event.name))
+        thread.start()
+    
+
+class MyDaemon(Daemon):
+    def run(self):
+        syslog.openlog('scribeClient',syslog.LOG_PID)
+        path = '/opt/logs/nginx/'
+        wm = WatchManager() 
+        mask = IN_DELETE | IN_CREATE |IN_MODIFY
+        notifier = Notifier(wm, EventHandler())
+        wm.add_watch(path, mask,rec=True)
+        #print 'now starting monitor %s'%(path)
+        while True:
+            try:
+                notifier.process_events()
+                if notifier.check_events():
+                    notifier.read_events()
+            except KeyboardInterrupt:
+                notifier.stop()
+                break
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+        daemon = MyDaemon('/tmp/daemon.pid')
+        if len(sys.argv) == 2:
+                if 'start' == sys.argv[1]:
+                        daemon.start()
+                elif 'stop' == sys.argv[1]:
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+                else:
+                        print "Unknown command"
+                        sys.exit(2)
+                sys.exit(0)
+        else:
+                print "usage: %s start|stop|restart" % sys.argv[0]
+                sys.exit(2)
+
+
